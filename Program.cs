ed07a4303b461d4e11287b8a7d37bb48b20f41d8
hamzafoy <hamzaoflouisville@gmail.com>
2022-02-01 17:48:43 -0500

    Added methods that rendered a string of the alphabet forward, backwards, and skipped intervals.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace GeneralProgramForCodeLouisville
 {
@@ -8,6 +9,78 @@ namespace GeneralProgramForCodeLouisville
         {
             CodeLouisvilleStudent hamzaFoy = new CodeLouisvilleStudent("Hamza", "Foy");
             hamzaFoy.introduceOneself();
+            RenderAlphabetQuicker();
+        }
+
+        private static void RenderAlphabetQuicker()
+        {
+            StringBuilder sb = new StringBuilder();
+            for (char letter = 'A'; letter <= 'Z'; letter++)
+            {
+                sb.Append(letter);
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+        private static void RenderAlphabetWithoutStringBuilder()
+        {
+            string alphabetString = "";
+            for (char i = char.MinValue; i < char.MaxValue; i++)
+            {
+                if (i >= 'A' && i <= 'Z')
+                {
+                    alphabetString += i.ToString();
+                }
+
+            }
+            Console.WriteLine(alphabetString);
+        }
+
+
+
+        private static void RenderAlphabet()
+        {
+            StringBuilder alphabetString = new StringBuilder();
+            for (char i = char.MinValue; i < char.MaxValue; i++)
+            {
+                if (i >= 'A' && i <= 'Z')
+                {
+                    alphabetString.Append(i);
+                }
+
+            }
+            Console.WriteLine(alphabetString.ToString());
+        }
+
+
+
+        private static void RenderAlphabetSkipped()
+        {
+            StringBuilder alphabetString = new StringBuilder();
+            char letter = 'A';
+            while (letter <= 'Z')
+            {
+                alphabetString.Append(letter);
+                letter++;
+                letter++;
+            }
+            Console.WriteLine(alphabetString.ToString());
+        }
+
+
+
+        private static void RenderAlphabetBackwards()
+        {
+            StringBuilder alphabetstring = new StringBuilder();
+            for (char i = char.MaxValue; i > char.MinValue; i--)
+            {
+                if (i >= 'A' && i <= 'Z')
+                {
+                    alphabetstring.Append(i);
+                }
+
+            }
+            Console.WriteLine(alphabetstring.ToString());
         }
     }
 
@@ -45,5 +118,7 @@ namespace GeneralProgramForCodeLouisville
         {
             throw new NotImplementedException();
         }
+
     }
+
 }

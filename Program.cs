eaa47109adc5a3c50429ed32cfa52f2a6284dace
hamzafoy <hamzaoflouisville@gmail.com>
2022-02-03 11:33:54 -0500

    Reduced lines of code, removed unnecessary lines

--- Program.cs
+++ Program.cs
@@ -8,17 +8,18 @@ namespace GeneralProgramForCodeLouisville
         static void Main(string[] args)
         {
             Console.Write("Type 1 to render alphabet A to Z, \ntype 2 to render alphabet Z to A, \ntype 3 to pick how many letters are skipped in rendering the alphabet: ");
-            string userChoiceToRender = Console.ReadLine();
-            string skippedLettersInput = null;
-            if (userChoiceToRender == "3")
+            int userChoiceToRender = Convert.ToInt32(Console.ReadLine());
+            string skippedLettersInput = "1";
+            if (userChoiceToRender == 3)
             {
                 Console.WriteLine("How many letters do you want to skip in rendering the alphabet?");
                 skippedLettersInput = Console.ReadLine();
             }
-            int userChoiceNumber = Convert.ToInt32(userChoiceToRender), skippedLettersNumber = Convert.ToInt32(skippedLettersInput);
+            int userChoiceNumber = (userChoiceToRender != null) ? userChoiceToRender : 1;
+            int skippedLettersNumber = Convert.ToInt32(skippedLettersInput);
             Console.WriteLine(RenderUserChosenAlphabet(userChoiceNumber, skippedLettersNumber));
         }
-        private static string RenderUserChosenAlphabet(int choice, int skip)
+        private static string RenderUserChosenAlphabet(int choice = 1, int? skip = 1)
         {
             StringBuilder alphabetString = new StringBuilder();
             switch(choice)
@@ -36,7 +37,7 @@ namespace GeneralProgramForCodeLouisville
                     }
                     break;
                 case 3:
-                    for (char letter = 'A'; letter <= 'Z'; letter = (char)(letter + skip))
+                    for (char letter = 'A'; letter <= 'Z'; letter = (char)(letter + skip??1))
                     {
                         alphabetString.Append(letter);
                     }
@@ -44,34 +45,5 @@ namespace GeneralProgramForCodeLouisville
             }
             return alphabetString.ToString();
         }
-        private static string RenderAlphabetQuicker()
-        {
-            StringBuilder alphabetString = new StringBuilder();
-            for (char letter = 'A'; letter <= 'Z'; letter++)
-            {
-                alphabetString.Append(letter);
-            }
-            return alphabetString.ToString();
-        }
-        private static string RenderAlphabetSkipped(int? userInputFixed = 1)
-        {
-            StringBuilder alphabetString = new StringBuilder();
-            for (char letter = 'A'; letter <= 'Z'; letter = (char)(letter + userInputFixed))
-            {
-                alphabetString.Append(letter);
-            }
-            return alphabetString.ToString();
-        }
-        private static string RenderAlphabetBackwards()
-        {
-            StringBuilder alphabetstring = new StringBuilder();
-            for (char letter = 'Z'; letter >= 'A'; letter--)
-            {
-                alphabetstring.Append(letter);
-            }
-            return alphabetstring.ToString();
-        }
-
     }
-
 }

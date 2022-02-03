bf8663c9b27f190dde8ea9c0d429bde74ebd2eef
hamzafoy <hamzaoflouisville@gmail.com>
2022-02-03 10:13:52 -0500

    Constructed a method to output either the Alphabet A-Z, alphabet Z-A, or an alphabet with x numbers skipped based on user input.

--- Program.cs
+++ Program.cs
@@ -7,23 +7,36 @@ namespace GeneralProgramForCodeLouisville
     {
         static void Main(string[] args)
         {
-            Console.Write("Type 1 to render alphabet A to Z, type 2 to render alphabet Z to A: ");
+            Console.Write("Type 1 to render alphabet A to Z, \ntype 2 to render alphabet Z to A, \ntype 3 to pick how many letters are skipped in rendering the alphabet: ");
             string userChoiceToRender = Console.ReadLine();
-            Console.WriteLine(RenderUserChosenAlphabet(userChoiceToRender = "1"));
+            string skippedLettersInput = null;
+            if (userChoiceToRender == "3")
+            {
+                Console.WriteLine("How many letters do you want to skip in rendering the alphabet?");
+                skippedLettersInput = Console.ReadLine();
+            }
+            int userChoiceNumber = Convert.ToInt32(userChoiceToRender), skippedLettersNumber = Convert.ToInt32(skippedLettersInput);
+            Console.WriteLine(RenderUserChosenAlphabet(userChoiceNumber, skippedLettersNumber));
         }
-        private static string RenderUserChosenAlphabet(string choice)
+        private static string RenderUserChosenAlphabet(int choice, int skip)
         {
             StringBuilder alphabetString = new StringBuilder();
             switch(choice)
             {
-                case "1":
+                case 1:
                     for(char letter = 'A'; letter <= 'Z'; letter++)
                     {
                         alphabetString.Append(letter);
                     }
                     break;
-                case "2":
+                case 2:
                     for(char letter = 'Z'; letter >= 'A'; letter--)
+                    {
+                        alphabetString.Append(letter);
+                    }
+                    break;
+                case 3:
+                    for (char letter = 'A'; letter <= 'Z'; letter = (char)(letter + skip))
                     {
                         alphabetString.Append(letter);
                     }
